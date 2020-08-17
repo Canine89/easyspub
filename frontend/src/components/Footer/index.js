dd4bb410c4f50cbc0e5c30047f15e89488d3de7f
canine <canine@canine.local>
2020-08-17 22:18:36 +0900

    [frontend] scss 도입 완료

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,18 +1,20 @@
 import React from "react";
-import styles from "./styles.scss";
+import "./styles.scss";
 
 const Footer = (props, context) => (
-  <footer className={styles.footer}>
-    <div className={styles.copyrights}>
-      <span className={styles.address}>
+  <footer className="footer">
+    <div className="copyrights">
+      <span className="address">
         서울특별시 마포구 잔다리로 109(우편번호 04003)
       </span>
-      <span className={styles.phonefax}>TEL (02)325-1722 FAX (02)326-1723</span>
-      <span className={styles.corpid}>사업자등록번호 105-87-44487</span>
-      <span className={styles.representer}>대표자 : 이지연</span>
-      <span className={styles.email}>Email : easy@easyspub.co.kr</span>
-      <span className={styles.copyright}>
-        Copyright(c)2015 이지스퍼블리싱㈜ EasysPublishing Co., Ltd. All Rights /
+      <span className="phonefax">TEL (02)325-1722 FAX (02)326-1723</span>
+      <br />
+      <span className="corpid">사업자등록번호 105-87-44487</span>
+      <span className="representer">대표자 : 이지연</span>
+      <span className="email">Email : easy@easyspub.co.kr</span>
+      <br />
+      <span className="copyright">
+        Copyright(c)2015 이지스퍼블리싱㈜ EasysPublishing Co., Ltd. All Rights
         Reserved
       </span>
     </div>
